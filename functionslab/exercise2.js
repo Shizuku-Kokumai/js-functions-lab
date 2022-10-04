07b5dd65e1f96817aca2ef5480c3c6fd32df2e7e
Shizuku Kokumai <bp20005@shibaura-it.ac.jp>
2022-10-05 01:06:33 +0900

    Exercise 1.

--- functionslab/exercise2.js
+++ functionslab/exercise2.js
@@ -1,7 +1,7 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
-    .then(todos => {
-        const completed = todos.filter((todo)=>todo.completed) // Complete this code
+    .then(json => {
+        const completed = null // Complete this code
         completed.forEach((todo, index) => {
             console.log(`${todo.title} - ${todo.completed}`)
         })
